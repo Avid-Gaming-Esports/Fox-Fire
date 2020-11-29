25be55ade6d6aa76a22e61d524f5ef74275f69a8
Justin Ang <justinang13@gmail.com>
2020-11-29 11:19:19 -0800

    added long polling for twitch stream. Removed ping/pong test command

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-
+const axios = require("axios")
 const client = new Discord.Client();
 
 const prefix = "!";
-
 const QUOTE_LIST = [
 	"Don't you trust me?",
 	"Play time's over.",
@@ -26,6 +25,53 @@ const QUOTE_LIST = [
 	"Should I make your pulse rise? Or... STOP!",
 ]
 
+const longPoll = function() {
+	// console.log("isActive: " + isActive);
+	const loc = client.channels.cache.get('772345686158082068');
+	const twImg1 = 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:' + 
+		'best,f_auto/wp-cms/uploads/2019/09/3-twitch-is-rebranding-for-the-first-time.jpg';
+	const twImg2 = 'https://blog.twitch.tv/assets/uploads/03-glitch.jpg';
+
+	axios.defaults.headers.common["Client-ID"] = config.TWITCH_ID;
+	axios.defaults.headers.common["Authorization"] = config.TWITCH_OAUTH;
+
+	axios
+		.get("https://api.twitch.tv/helix/search/channels?query=avidgamingesports")
+		.then(response => {
+			// console.log(response.data.data[0].is_live)
+			// console.log(client.user.username)
+			if(!isActive) {
+				currStream = response.data.data[0]
+				if(currStream.is_live) {
+					// Send the message
+					const linkEmbed = new Discord.MessageEmbed()
+					.setColor('#0099ff')
+					.setTitle(currStream.title)
+					.setURL('https://www.twitch.tv/avidgamingesports')
+					.setAuthor('avidgamingesports', twImg1, 
+						'https://www.twitch.tv/avidgamingesports')
+					.setThumbnail(twImg2)
+					.setTimestamp()
+					loc.send('AvidGamingEsports is now live! ' + `@everyone`)
+					loc.send(linkEmbed)
+				}
+			}
+			isActive = response.data.data[0].is_live
+
+			// console.log(response.data.data);
+			// console.log(response.data.data.length);
+		})
+		.catch(error => {
+			console.log(error);
+		});
+}
+
+let isActive = false
+
+client.on('ready', function() {
+  client.setInterval(longPoll, 60000);
+});
+
 client.on("message", function (message) {
 	if (message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
@@ -33,11 +79,6 @@ client.on("message", function (message) {
 	const commandBody = message.content.slice(prefix.length);
 	const args = commandBody.split(' ');
 	const command = args.shift().toLowerCase();
-
-	if (command === "ping") {
-		const timeTaken = Date.now() - message.createdTimestamp;
-		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-	}
 
 	if (command === "autoassign") {
 		// let role = message.guild.roles.cache.find(r => r.name === "Member");
