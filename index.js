a2ae259e0a3e31334988552f6e541c0a5f6434b7
Justin Ang <justinang13@gmail.com>
2020-12-11 17:30:00 -0800

    Added Google Sheets integration with current power-rankings. Only updated up to week 2. Command will now pull from sheet and display relevant information (has not been heavily tested.)

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const Discord = require("discord.js");
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { promisify } = require('util');
+
+const creds = require('./client_secret.json');
 const config = require("./config.json");
-const axios = require("axios")
+
+const axios = require("axios");
+const { strict } = require("assert");
 const client = new Discord.Client();
 
 const prefix = "!";
@@ -27,6 +33,7 @@ const QUOTE_LIST = [
 
 const longPoll = function() {
 	// console.log("isActive: " + isActive);
+
 	const loc = client.channels.cache.get('772345686158082068');
 	const twImg1 = 'https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:' + 
 		'best,f_auto/wp-cms/uploads/2019/09/3-twitch-is-rebranding-for-the-first-time.jpg';
@@ -38,12 +45,9 @@ const longPoll = function() {
 	axios
 		.get("https://api.twitch.tv/helix/search/channels?query=avidgamingesports")
 		.then(response => {
-			// console.log(response.data.data[0].is_live)
-			// console.log(client.user.username)
 			if(!isActive) {
 				currStream = response.data.data[0]
 				if(currStream.is_live) {
-					// Send the message
 					const linkEmbed = new Discord.MessageEmbed()
 					.setColor('#0099ff')
 					.setTitle(currStream.title)
@@ -57,12 +61,9 @@ const longPoll = function() {
 				}
 			}
 			isActive = response.data.data[0].is_live
-
-			// console.log(response.data.data);
-			// console.log(response.data.data.length);
 		})
-		.catch(error => {
-			console.log(error);
+		.catch(_error => {
+			console.log("Failed to fetch Twitch API.");
 		});
 }
 
@@ -72,7 +73,7 @@ client.on('ready', function() {
   client.setInterval(longPoll, 60000);
 });
 
-client.on("message", function (message) {
+client.on("message", async function (message) {
 	if (message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
 
@@ -93,6 +94,38 @@ client.on("message", function (message) {
 		message.channel.send(quote);
 	}
 
+	if (command === "powerrank") {
+		const res = await accessSpreadSheet();
+		let resp = "";
+		let roles = ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "SUPPORT"]
+		if (args[0] === "all") {
+			resp += "Pulling top 25 power rankings: " + "\n" + 
+				"#####################################";
+			for(let i = 1; i < 26; i++) {
+				let idx = i.toString();
+				let role = res[idx][2].charAt(0).toUpperCase() + res[idx][2].toLowerCase().slice(1)
+				resp += "\n" + idx + " : " + res[idx][0] + ' (' + role + ') ' + ' (' + res[idx][1] + ') '
+			}
+		} else if (roles.includes(args[0].toUpperCase())) {
+			let curr = 1
+			let role = args[0].toUpperCase()
+			resp += "Pulling top 5 players in " + role + "\n"
+			let filtered = Object.keys(res).reduce(function (filtered, key) {
+				if (res[key][2] === role){
+					filtered[curr] = res[key]
+					curr += 1
+				}
+				return filtered;
+			}, {});
+			for(let i = 1; i < 6; i++) {
+				let idx = i.toString();
+				let role = filtered[idx][2].charAt(0).toUpperCase() + filtered[idx][2].toLowerCase().slice(1)
+				resp += "\n" + idx + " : " + filtered[idx][0] + ' (' + role + ') '
+			}
+		}
+		message.channel.send(resp)
+	}
+
 });
 
 client.on('guildMemberAdd', (guildMember) => {
@@ -103,3 +136,26 @@ client.on('guildMemberAdd', (guildMember) => {
 });
 
 client.login(config.BOT_TOKEN);
+
+async function accessSpreadSheet() {
+	const doc = new GoogleSpreadsheet('1oiKPuIbBt1_4U8IC0jIKz9_IMl4V5_PAqA_jrhdAsuo');
+  await doc.useServiceAccountAuth({
+    client_email: creds.client_email,
+    private_key: creds.private_key,
+  });
+  await doc.loadInfo();
+
+	const sheet = doc.sheetsById[1076286390];
+	const rows = await sheet.getRows({
+		offset: 4,
+		limit: 100
+	})
+
+	res = {}
+
+	rows.forEach((row) => {
+		let raws = row._rawData
+		res[raws[5]] = [raws[1], raws[2], raws[3]]
+	})
+	return res
+}
